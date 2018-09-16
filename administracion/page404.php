ea63df72d7cc7267fdabc7e82939f686dc09c588
Xavi Moreno <xaviermorenoaviles@gmail.com>
2018-09-15 23:52:13 -0500

    añadiendo nuevos estilos a la pagina de error

--- administracion/page404.php
+++ administracion/page404.php
@@ -1,12 +1,66 @@
-<!doctype html>
-<html>
-<head>
-    <meta charset="UTF-8">
-    <title>Untitled Document</title>
-</head>
-<body>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <!-- Meta, title, CSS, favicons, etc. -->
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <h1>Error amigo administrador</h1>
+    <title>Administracion | Error 404</title>
 
-</body>
+    <!-- Bootstrap -->
+    <link href="http://www.marketton.com/administracion/vendors/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
+    <!-- Font Awesome -->
+    <link href="http://www.marketton.com/administracion/vendors/font-awesome/css/font-awesome.min.css" rel="stylesheet">
+    <!-- NProgress -->
+    <link href="http://www.marketton.com/administracion/vendors/nprogress/nprogress.css" rel="stylesheet">
+
+    <!-- Custom Theme Style -->
+    <link href="http://www.marketton.com/administracion/build/css/custom.min.css" rel="stylesheet">
+  </head>
+
+  <body class="nav-md">
+    <div class="container body">
+      <div class="main_container">
+        <!-- page content -->
+        <div class="col-md-12">
+          <div class="col-middle">
+            <div class="text-center text-center">
+              <h1 class="error-number">404</h1>
+              <h2>Lo sentimos pero no pudimos encontrar esta página</h2>
+              <p>Esta página que estás buscando no existe <a href="#">¿Reporta esto?</a>
+              </p>
+              <div class="mid_center">
+              <a href="http://www.marketton.com/administracion/">Regresar al inicio!</a>
+                <!--<form>
+                  <div class="col-xs-12 form-group pull-right top_search">
+                    <div class="input-group">
+                      <input type="text" class="form-control" placeholder="Search for...">
+                      <span class="input-group-btn">
+                              <button class="btn btn-default" type="button"><a href="https://www.sisprologsa.com.ec/sax/">Regresar al inicio!</a></button>
+                          </span>
+                    </div>
+                  </div>
+                </form>-->
+              </div>
+            </div>
+          </div>
+        </div>
+        <!-- /page content -->
+      </div>
+    </div>
+
+    <!-- jQuery -->
+    <script src="http://www.marketton.com/administracion/vendors/jquery/dist/jquery.min.js"></script>
+    <!-- Bootstrap -->
+    <script src="http://www.marketton.com/administracion/vendors/bootstrap/dist/js/bootstrap.min.js"></script>
+    <!-- FastClick -->
+    <script src="http://www.marketton.com/administracion/vendors/fastclick/lib/fastclick.js"></script>
+    <!-- NProgress -->
+    <script src="http://www.marketton.com/administracion/vendors/nprogress/nprogress.js"></script>
+
+    <!-- Custom Theme Scripts -->
+    <script src="http://www.marketton.com/administracion/build/js/custom.min.js"></script>
+  </body>
 </html>
